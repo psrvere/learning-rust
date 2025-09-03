51b8c1c8be2c1b73be8c7cd0a2d87f73602b48bd
Prateek Singh Rathore <prateek.singh.rathore@gmail.com>
2025-09-03 14:29:13 +0530

    Completed Chapter12
      - build a migigrep cli prgram
    
    Signed-off-by: Prateek Singh Rathore <prateek.singh.rathore@gmail.com>

--- Chapter12/src/main.rs
+++ Chapter12/src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, process};
+use minigrep::{Config, run};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
