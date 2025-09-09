78b03ad4c4a97ec2a9fa58cb94b9b7a5f6042836
Prateek Singh Rathore <prateek.singh.rathore@gmail.com>
2025-09-09 11:28:35 +0530

    Added memory leak example
    Signed-off-by: Prateek Singh Rathore <prateek.singh.rathore@gmail.com>

--- Chapter15/src/main.rs
+++ Chapter15/src/main.rs
@@ -1,9 +1,11 @@
 mod box_examples;
 mod rc_examples;
 mod refcell_examples;
+mod memory_leak;
 
 fn main() {
     box_examples::examples();
     rc_examples::examples();
     refcell_examples::examples();
+    memory_leak::examples();
 }
---
+++ Chapter15/src/memory_leak.rs
@@ -0,0 +1,51 @@
+use std::cell::RefCell;
+
+pub fn examples() {
+    reference_cycle();
+}
+
+// Reference Cycles can Leak Memory
+
+// Creating a reference cycle
+
+fn reference_cycle() {
+    println!("*** reference_cycle ***");
+    use std::rc::Rc;
+    use List::{Cons, Nil};
+
+    #[derive(Debug)]
+    enum List {
+        Cons(i32, RefCell<Rc<List>>),
+        Nil,
+    }
+
+    impl List {
+        fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+            match self {
+                Cons(_, item) => Some(item),
+                Nil => None,
+            }
+        }
+    }
+
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment this line to see that we have a cycle.
+    // This will overflow the stack.
+    // println!("a next item = {:?}", a.tail());
+}
